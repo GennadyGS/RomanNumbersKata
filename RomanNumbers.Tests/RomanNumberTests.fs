005097da39d496105290bc362d67edea07bd94f9
Gennadii Saltyshchak <GennadyGS@gmail.com>
2015-01-23 17:49:54 +0200

    Range limit test red

--- RomanNumbers.Tests/RomanNumberTests.fs
+++ RomanNumbers.Tests/RomanNumberTests.fs
@@ -8,6 +8,13 @@ open RomanNumbers.RomanNumbers
 type RomanNumbersTests() =
     
     [<Theory>]
+    [<InlineData(-1)>]
+    [<InlineData(0)>]
+    [<InlineData(1001)>]
+    let ``Should raise exception when number not within acceptable range``number =
+        number |> toRomanNumber |> should throw typeof<System.ArgumentOutOfRangeException>
+
+    [<Theory>]
     [<InlineData(1, "I")>]
     let ``Should return correct result``(number, romanNumber) =
         number |> toRomanNumber |> should equal romanNumber
