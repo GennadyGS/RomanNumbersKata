df90e9a6c156bbdaa6363096d04d04c57490ea67
Gennadii Saltyshchak <GennadyGS@gmail.com>
2015-01-23 18:12:04 +0200

    Test for 2 refactoring

--- RomanNumbers/RomanNumbers.fs
+++ RomanNumbers/RomanNumbers.fs
@@ -4,4 +4,4 @@ module RomanNumbers =
     let toRomanNumber number = 
         if number <= 0 || number > 3999 then
             raise (System.ArgumentOutOfRangeException(number |> sprintf "Number %i cannot be represented as roman number"))
-        new string('I', number)
+        "I" |> String.replicate number
