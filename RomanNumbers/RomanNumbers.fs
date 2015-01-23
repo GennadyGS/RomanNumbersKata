e5d4afb8ade36219a4d03dd1972c41228bdca2b0
Gennadii Saltyshchak <GennadyGS@gmail.com>
2015-01-23 18:19:39 +0200

    Test for 5 green

--- RomanNumbers/RomanNumbers.fs
+++ RomanNumbers/RomanNumbers.fs
@@ -4,4 +4,7 @@ module RomanNumbers =
     let toRomanNumber number = 
         if number <= 0 || number > 3999 then
             raise (System.ArgumentOutOfRangeException(number |> sprintf "Number %i cannot be represented as roman number"))
-        "I" |> String.replicate number
+        if number = 5 then 
+            "V"
+        else
+            "I" |> String.replicate number
