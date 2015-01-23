1899ddea8a3921b8a10c51dac58996b23f086129
Gennadii Saltyshchak <GennadyGS@gmail.com>
2015-01-23 18:25:45 +0200

    Test for 5 refactoring

--- RomanNumbers/RomanNumbers.fs
+++ RomanNumbers/RomanNumbers.fs
@@ -4,7 +4,7 @@ module RomanNumbers =
     let toRomanNumber number = 
         if number <= 0 || number > 3999 then
             raise (System.ArgumentOutOfRangeException(number |> sprintf "Number %i cannot be represented as roman number"))
-        if number = 5 then 
+        if number >= 5 then 
             "V"
         else
             "I" |> String.replicate number
