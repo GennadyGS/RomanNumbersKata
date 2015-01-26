4153ff426cf0f0ad3cf4859659ddf1792e97d7e5
Gennadii Saltyshchak <GennadyGS@gmail.com>
2015-01-26 11:49:26 +0200

    Test for 40 green

--- RomanNumbers/RomanNumbers.fs
+++ RomanNumbers/RomanNumbers.fs
@@ -2,40 +2,48 @@
 
 module RomanNumbers = 
     [<Literal>]
-    let One = 1
-
-    [<Literal>]
-    let OneRomanDigit = 'I'
-
-    [<Literal>]
-    let Five = 5
-    
-    [<Literal>]
-    let FiveRomanDigit = 'V'
-
-    [<Literal>]
     let Ten = 10
     
     [<Literal>]
-    let TenRomanDigit = 'X'
+    let Five = 5
+
+    let RomanDigitsDefs = [ ('I', 'V'); ('X', 'L')]
 
     let ToRomanNumber number = 
 
-        let DuplicateOneDigits number romanDigit = 
-            new string(romanDigit, number)
+        let ToRomanDigit digit (oneRomanDigit, fiveRomanDigit, someTenRomanDidit) = 
 
-        let ToRomanDigit digit = 
+            let DuplicateOneDigits number romanDigit = 
+                new string(romanDigit, number)
+
             match digit with
-            | 9 -> string OneRomanDigit + string TenRomanDigit
-            | 5 | 6 | 7 | 8 -> string FiveRomanDigit + DuplicateOneDigits (digit - Five) OneRomanDigit
-            | 4 -> string OneRomanDigit + string FiveRomanDigit
-            | 0 | 1 | 2 | 3 -> DuplicateOneDigits digit OneRomanDigit   
+            | 9 -> 
+                match someTenRomanDidit with
+                | Some(tenRomanDigit) -> string oneRomanDigit + string tenRomanDigit    
+                | None -> raise (System.ArgumentOutOfRangeException())
+            | 5 | 6 | 7 | 8 -> string fiveRomanDigit + DuplicateOneDigits (digit - Five) oneRomanDigit
+            | 4 -> string oneRomanDigit + string fiveRomanDigit
+            | 0 | 1 | 2 | 3 -> DuplicateOneDigits digit oneRomanDigit   
             | _ -> raise (System.ArgumentOutOfRangeException())
-        
-        if number <= 0 || number > 3999 then
-            raise (System.ArgumentOutOfRangeException(number |> sprintf "Number %i cannot be represented as roman number"))
-        
-        let units = number % Ten
-        let tens = (number - units) / 10
 
-        DuplicateOneDigits tens TenRomanDigit + ToRomanDigit units
+        let rec ToRomanNumberByDigitDefs number romanDigitsDefs = 
+            
+            let ToRomanNumberByUnitsAndTensDigitDefs number unitsRomanDigitsDef tensRomanDigitsDef = 
+                    let units = number % Ten
+                    let tens = (number - units) / Ten
+                    let unitsRomanDigits = ToRomanDigit units unitsRomanDigitsDef
+                    let tensRomanDigits = ToRomanNumberByDigitDefs tens tensRomanDigitsDef
+                    tensRomanDigits + unitsRomanDigits
+
+            if number > 0 then
+                match romanDigitsDefs with
+                | (oneRomanDigit, fiveRomanDigit) :: (tenRomanDigit, fiftyRomanDigit) :: hundredsRomanDigits -> 
+                    ToRomanNumberByUnitsAndTensDigitDefs number (oneRomanDigit, fiveRomanDigit, Some(tenRomanDigit)) ((tenRomanDigit, fiftyRomanDigit) :: hundredsRomanDigits)
+                | (oneRomanDigit, fiveRomanDigit) :: tensRomanDigits -> 
+                    ToRomanNumberByUnitsAndTensDigitDefs number (oneRomanDigit, fiveRomanDigit, None) tensRomanDigits
+                | [] -> raise (System.ArgumentOutOfRangeException())
+            else System.String.Empty
+        
+        if number <= 0 then
+            raise (System.ArgumentOutOfRangeException(number |> sprintf "Number %i cannot be represented as roman number"))
+        ToRomanNumberByDigitDefs number RomanDigitsDefs
